61fb9b105eb67c664c44008584a1aafb948ce68b
lgLuis <65200017+lgLuis@users.noreply.github.com>
2021-09-02 13:02:38 -0300

    CONTACTO - FORMULARIO - VALIDACION

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // Add Your Scripts here
+
+(function(){
+    'use strict';
+    window.addEventListener('load', function(){
+        //SELECCIONAR EL FORMULARIO PARA VALIDAR
+        var forms = this.document.getElementsByClassName('needs-validation');
+        // VALIDAD CADA CAMPO Y PREVENIR QUE SE ENVIE
+        var validation = Array.prototype.filter.call(forms, function(form){
+            form.addEventListener('submit', function(event){
+                if(form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
